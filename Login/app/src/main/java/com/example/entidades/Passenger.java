e3ac225286842e5b9c57ab4f6acd9e261667c3ba
Alexis <170123@upslp.edu.mx>
2020-05-27 08:07:20 -0500

    Correción conexión

---
+++ Login/app/src/main/java/com/example/entidades/Passenger.java
@@ -0,0 +1,62 @@
+package com.example.entidades;
+
+public class Passenger {
+    private Integer id;
+    private String name;
+    private String last_name;
+    private String user;
+    private String pass;
+    private String email;
+
+    public Passenger() {
+
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
--- Login/app/src/main/java/com/example/login/MainActivity.java
+++ Login/app/src/main/java/com/example/login/MainActivity.java
@@ -2,31 +2,49 @@ package com.example.login;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
-    ConnectionMySQL SQL=new ConnectionMySQL(this);
-    Connection con;
-    TextView txtEmail;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.example.entidades.Passenger;
+import com.example.menu.User;
+import com.example.registro.Registration;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, Response.Listener<JSONObject>,Response.ErrorListener{
+    TextView txtUser;
     TextView txtPass;
     TextView txtReg;
+    Button btnLog;
+    ProgressDialog progreso;
+    RequestQueue request;
+    Boolean exist=false;
+    JsonObjectRequest jsonObjectRequest;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         txtReg=findViewById(R.id.lblRegistration);
         txtReg.setOnClickListener(this);
-        txtEmail=findViewById(R.id.txtEmail);
+        txtUser=findViewById(R.id.txtUser);
         txtPass=findViewById(R.id.txtPass);
-
+        btnLog=findViewById(R.id.btnLog);
+        request= Volley.newRequestQueue(this);
     }
 
     @Override
@@ -34,51 +52,66 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         switch (v.getId()) {
 
             case R.id.btnLog://Si se presiona ingresar
-                boolean band=login();
-                if (band){
-                    Toast toast = Toast.makeText(this, "Bienvenido", Toast.LENGTH_SHORT);
-                    toast.show();
-                }
-                else {
-                    Toast toast = Toast.makeText(this, "Usuario incorrecto", Toast.LENGTH_SHORT);
-                    toast.show();
-                }
-                break;
-
-            case R.id.lblRegistration: //En caso de que seleccione el texto de registrarse
-                    Intent intent = new Intent (v.getContext(), Registration.class);
+                login();
+                if(exist){
+                    Intent intent = new Intent(v.getContext(), User.class);
                     startActivityForResult(intent, 0);
+                    progreso.hide();
+                }
+                else if(!exist){
+                    Toast toast=Toast.makeText(this,"Alguno de los datos es incorrecto",Toast.LENGTH_SHORT);
+                }
                 break;
-
+            case R.id.lblRegistration: //En caso de que seleccione el texto de registrarse
+                Intent intent = new Intent(v.getContext(), Registration.class);
+                startActivityForResult(intent, 0);
+                break;
             default:
                 break;
         }
     }
-    public boolean login() {
-        try {
-            con = null;
-            con =  SQL.getConnection();
-            ResultSet res;
-            try (PreparedStatement ps = con.prepareStatement("SELECT * FROM passenger WHERE email=? AND password=?;")) {
-                ps.setString(1, txtEmail.getText().toString());
-                ps.setString(2, txtPass.getText().toString());
-                res = ps.executeQuery();
-            }
-            if (res.next()) {
-                return true;
-            }
-        } catch (SQLException ex) {
-            Toast toast = Toast.makeText(this, ex.toString(), Toast.LENGTH_SHORT);
-            toast.show();
-        } catch (NullPointerException ex) {
-            Toast toast = Toast.makeText(this, ex.toString(), Toast.LENGTH_SHORT);
-            toast.show();
-        } catch (IllegalStateException ex) {
-            Toast toast = Toast.makeText(this, ex.toString(), Toast.LENGTH_SHORT);
-            toast.show();
-        }
-        return false;
+    public void login() {
+        progreso=new ProgressDialog(this);
+        progreso.setMessage("Cargando...");
+        progreso.show();
+        String ip="https://ingsoftu1.000webhostapp.com/";
+        String url=ip+"login_usuario.php?user="+txtUser.getText().toString() +"&pass="+txtPass.getText().toString();
+        jsonObjectRequest=new JsonObjectRequest(Request.Method.GET,url,null,this,this);
+        request.add(jsonObjectRequest);
     }
 
+    @Override
+    public void onErrorResponse(VolleyError error) {
+        Toast.makeText(this,"No se pudo registrar",Toast.LENGTH_SHORT).show();
+        Log.i("ERROR",error.toString());
+
+    }
+
+    @Override
+    public void onResponse(JSONObject response) {
+        Passenger me =new Passenger();
+        JSONArray json=response.optJSONArray("usuario");
+        JSONObject jsonObject=null;
+        try{
+            jsonObject=json.getJSONObject(0);
+            me.setName(jsonObject.optString("name"));
+            me.setEmail(jsonObject.optString("email"));
+            me.setLast_name(jsonObject.optString("last_name"));
+            me.setUser(jsonObject.optString("user"));
+            me.setPass(jsonObject.optString("pass"));
+            me.setId(jsonObject.getInt("id"));
+        }
+        catch (JSONException e){
+            e.printStackTrace();
+        }
+        if(me.getId()!=0){
+            exist=true;
+            Toast.makeText(this,"Bienvenido "+me.getName(),Toast.LENGTH_SHORT).show();
+        }
+        else{
+            exist=false;
+        }
+
+    }
 }
 
